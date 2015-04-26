0561f47711391b1bffc820529b1e71e339f8dc2a
GodMod <GodMod@users.noreply.github.com>
2015-04-26 18:53:04 +0200

    Update ffxiv.class.php

--- ffxiv.class.php
+++ ffxiv.class.php
@@ -51,7 +51,7 @@ if(!class_exists('ffxiv')) {
 					'faction' => array(
 						'gridania'	=> 'all',
 						'limsa'		=> 'all',
-						'Uldah'		=> 'all',
+						'uldah'		=> 'all',
 					),
 				),
 			),
