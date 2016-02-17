188a57cf39985afe66c536d3caf5713153d50963
Simon W <s.wallmann@kompsoft.de>
2016-02-17 22:04:45 +0100

    bump version

--- ffxiv.class.php
+++ ffxiv.class.php
@@ -26,7 +26,7 @@ if ( !defined('EQDKP_INC') ){
 if(!class_exists('ffxiv')) {
 	class ffxiv extends game_generic {
 		protected static $apiLevel	= 20;
-		public $version				= '3.1.0';
+		public $version				= '3.1.1';
 		protected $this_game		= 'ffxiv';
 		protected $types			= array('classes', 'races', 'factions', 'filters');
 		protected $classes			= array();
@@ -77,7 +77,7 @@ if(!class_exists('ffxiv')) {
 				),
 			),
 		);
-		
+
 		protected $class_colors = array(
 			0	=> '#808080',
 			1	=> '#808080',
