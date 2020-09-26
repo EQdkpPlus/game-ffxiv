aa4bda13a4699520dd8abd6d4b29698ab0add4b8
Bloodsoul <Bloodsoul@users.noreply.github.com>
2020-09-26 16:04:17 +0200

    Add constants file

---
+++ constants.php
@@ -0,0 +1,52 @@
+<?php
+/* Project: EQdkp-Plus
+ * Package: Final Fantasy IV game package
+ * Link: http://eqdkp-plus.eu
+ *
+ * Copyright (C) 2006-2015 EQdkp-Plus Developer Team
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+define('UNKNOWN', 0);
+
+# races
+define('ELEZEN', 1);
+define('ROEGADYN', 2);
+define('HYUR', 3);
+define('MIQOTE', 4);
+define('LALAFELL', 5);
+define('AURA', 6);
+define('VIERA', 7);
+define('HROTHGAR', 8);
+
+# classes
+define('BLACK_MAGE', 1);
+define('WARRIOR', 2);
+define('DRAGOON', 3);
+define('MONK', 4);
+define('PALADIN', 5);
+define('BARD', 6);
+define('WHITE_MAGE', 7);
+define('SUMMONER', 8);
+define('SCHOLAR', 9);
+define('NINJA', 10);
+define('DARK_KNIGHT', 11);
+define('ASTROLOGIAN', 12);
+define('MACHINIST', 13);
+define('SAMURAI', 14);
+define('RED_MAGE', 15);
+define('GUNBREAKER', 16);
+define('DANCER', 17);
+define('BLUE_MAGE', 18);
